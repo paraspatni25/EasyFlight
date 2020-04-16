ad431ba437954fde68e30b4fd5823d51ba381891
Paras_patni <paras.d.patni@gmail.com>
2020-04-16 00:33:25 -0400

    Added card view and toggle button in  dashboard layout

---
+++ app/src/main/java/com/example/easyflight/DataBase.java
@@ -0,0 +1,17 @@
+package com.example.easyflight;
+
+import java.util.ArrayList;
+
+public class DataBase {
+
+    public ArrayList<String> AirPortCods;
+
+    public DataBase() {
+        AirPortCods = new ArrayList<>();
+
+        AirPortCods.add("DEL");
+        AirPortCods.add("DEL");
+        AirPortCods.add("DEL");
+        AirPortCods.add("DEL");
+    }
+}
